06c6a9adbf680846244205bdc5f9931dd49ec410
Joe Froula <jmfroula@gmail.com>
2023-05-10 08:53:38 -0700

    initial

---
+++ dist/services/stringService.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.logger = void 0;
+const logger = () => {
+    return 'string';
+};
+exports.logger = logger;
+//# sourceMappingURL=stringService.js.map
---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import { logger } from './services/stringService';
+import cors from 'cors'; 
+
+
+const app = express();
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+  res.send(logger());
+});
+
+app.get('/test', (req, res) => { 
+  res.send('test');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000'); 
+});    
